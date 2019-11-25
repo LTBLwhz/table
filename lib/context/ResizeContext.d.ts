47f2601f7a8aaa4b9154d37c54bccf29abaa58d3
wh.zhao <wh.zhao@ctrip.com>
2019-11-25 22:06:01 +0800

    compile

--- lib/context/ResizeContext.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface ResizeContextProps {
-    onColumnResize: (colIndex: number, width: number) => void;
-}
-declare const ResizeContext: import("react").Context<ResizeContextProps>;
-export default ResizeContext;
